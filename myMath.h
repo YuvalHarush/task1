783043782cc605609c418ce64648e4784e31a6b0
YuvalHarush <57765334+YuvalHarush@users.noreply.github.com>
2019-11-14 18:31:54 +0200

    Delete myMath.h

--- myMath.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-float add (double x, double y);
-float sub (double x, double y);
-float mul (double x, double y);
-float divi (double x, double y);
-float expo (int x);
-float power (double x, int y);
